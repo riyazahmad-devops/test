574a9b76743014f6b4d402b1287c1978863b3f75
riyazahmad-devops <riyaz.nep@gmail.com>
2021-05-30 23:34:06 +0545

    riyaz vai again

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! from riyaz";
+    private final String message = "Hello World! from riyaz lol to lol";
 
     public App() {}
 
